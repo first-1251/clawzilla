0d4aee50e508d8398d03b3a394056e5b6640256b
Name <koipasta3@gmail.com>
2018-01-27 14:44:25 -0500

    - Added Range map - Minor tweaks
    - Add encoder functionality
    - Move range map to command?
    - Boop?

--- src/main/java/org/usfirst/frc/team1251/robot/subsystems/Elevator.java
+++ src/main/java/org/usfirst/frc/team1251/robot/subsystems/Elevator.java
@@ -6,6 +6,11 @@ import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team1251.robot.RobotMap;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by nick2 on 1/18/18
  */
@@ -14,18 +19,22 @@ public class Elevator extends Subsystem {
     // Motor(s) for elevators (move it up and down)
     private Victor elevatorMotor1;
     //Maintains position (counteracts gravity) while holding crate at limit switch
-    //TODO: FIND CORRECT LS IDLING SPEED
+    //TODO: FIND CORRECT LIMIT SWITCH IDLING SPEED
     private final static double LIMIT_SWITCH_IDLE_SPEED = 0;
     // Encoder for elevator
     private Encoder elevatorEncoder;
     //TODO: FIGURE OUT WHAT TO DO WITH ENCODER ¯\_(ツ)_/¯
     // Limit Switches that prevent the elevator from overextending
     private DigitalInput elevatorLimitSwitch;
+    private Map<Double[], Double> rangeMap = new HashMap<>();
 
     public Elevator(){
         elevatorEncoder = new Encoder(RobotMap.ELEVATOR_ENCODER_PORT1, RobotMap.ELEVATOR_ENCODER_PORT2);
         elevatorMotor1 = new Victor(RobotMap.ELEVATOR_VICTOR);
         elevatorLimitSwitch = new DigitalInput(RobotMap.ELEVATOR_LIMIT_SWITCH);
+        rangeMap.put(new Double[]{1.0, 0.0}, 0.0);
+        rangeMap.put(new Double[]{0.0, -0.1}, 0.1);
+        rangeMap.put(new Double[]{-0.1}, 1.0);
     }
 
     public void initDefaultCommand() {
@@ -55,6 +64,12 @@ public class Elevator extends Subsystem {
         if (elevatorLimitSwitch.get()){
             elevatorMotor1.set(0);
         }
+    }
+
+    //TODO: CREATE RANGE MAP FOR SPEEDS
+    public double map(double[][] range, double joystickPosition){
+        double speed = 0;
+        return speed;
     }
 }
 
