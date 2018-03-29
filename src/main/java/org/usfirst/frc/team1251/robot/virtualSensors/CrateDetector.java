c8b9d98962cab430657ba1bf3154b19d0128d49e
Chris Russell <crussell@sonnysdirect.com>
2018-03-29 10:09:44 -0400

    Report recently collected crates as collected

--- src/main/java/org/usfirst/frc/team1251/robot/virtualSensors/CrateDetector.java
+++ src/main/java/org/usfirst/frc/team1251/robot/virtualSensors/CrateDetector.java
@@ -14,17 +14,64 @@ public class CrateDetector {
     private Timer buttonTimer = new Timer();
     private boolean isTimerRunning = false;
 
+    private Timer recentlyCollectedTimer = new Timer();
+    private boolean isRecentlyCollectedTimerRunning = false;
+
     public CrateDetector() {
         collectorSwitch = new DigitalInput(RobotMap.COLLECTOR_SWITCH);
     }
 
+    private void markAsRecentlyCollected() {
+        if (isRecentlyCollectedTimerRunning) {
+            recentlyCollectedTimer.stop();
+        }
+
+        recentlyCollectedTimer.reset();
+        recentlyCollectedTimer.start();
+        isRecentlyCollectedTimerRunning = true;
+    }
+
+    private boolean isRecentlyCollected() {
+        if (!isRecentlyCollectedTimerRunning) {
+            return false;
+        }
+
+        // Timer expires after a second.
+        if (recentlyCollectedTimer.get() >= 1.0) {
+            // Timer has expired, stop and reset it then return false. Crate was detected but not recently *enough*.
+            recentlyCollectedTimer.stop();
+            recentlyCollectedTimer.reset();
+            isRecentlyCollectedTimerRunning = false;
+            return false;
+        }
+
+        // Recently collected timer is running and has not expired.
+        return true;
+    }
+
+    private boolean isButtonPressed() {
+        return !collectorSwitch.get();
+    }
+
     public boolean isCrateCollected()
     {
-        if (!collectorSwitch.get())
+        // If the crate was recently collected, there is no reason to check the button state.
+        if (isRecentlyCollected()) {
+            return true;
+        }
+
+        if (isButtonPressed())
         {
             if (isTimerRunning)
             {
-                return buttonTimer.get() >= 1;
+                // Timer is running... see if the button has been held long enough.
+                if (buttonTimer.get() >= .10) {
+                    // Button has been held long enough. Mark the crate as recently collected and return true.
+                    markAsRecentlyCollected();
+                    return true;
+                } else {
+                    return false;
+                }
 
             } else {
                 buttonTimer.start();
@@ -33,12 +80,16 @@ public class CrateDetector {
             }
 
         } else {
+            // Button is not pressed...
 
+            // stop and reset the timer if it is running.
             if (isTimerRunning) {
                 buttonTimer.stop();
                 buttonTimer.reset();
                 isTimerRunning = false;
             }
+
+            // Indicate that the crate is not collected.
             return false;
 
         }
