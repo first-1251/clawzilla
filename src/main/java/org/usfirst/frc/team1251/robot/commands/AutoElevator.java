7f9588f2f8e3689f4b9f26385605deba32d31af8
Name <koipasta3@gmail.com>
2018-03-29 09:48:05 -0400

    Applied the AutoArmTo90 Code to the elevator (uses elevator, elevatorPosition, and height in inches)
    NEED TO FIX ELEVATOR GETHEIGHT MATH!

--- src/main/java/org/usfirst/frc/team1251/robot/commands/AutoElevator.java
+++ src/main/java/org/usfirst/frc/team1251/robot/commands/AutoElevator.java
@@ -1,10 +1,46 @@
 package org.usfirst.frc.team1251.robot.commands;
 
+import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team1251.robot.subsystems.Elevator;
 import org.usfirst.frc.team1251.robot.subsystems.ElevatorShifter;
+import org.usfirst.frc.team1251.robot.virtualSensors.ElevatorPosition;
 
-public class AutoElevator {
+public class AutoElevator extends Command {
 
     private Elevator elevator;
-    private ElevatorShifter elevatorShifter;
+    private ElevatorPosition elevatorPosition;
+
+    // Desired height in inches
+    private int height;
+
+    private double speed = 0.8;
+    private double sustainSpeed = 0.10;
+    private boolean isAtPosition;
+
+    public AutoElevator(Elevator elevator, ElevatorPosition elevatorPosition, int height) {
+        this.elevator = elevator;
+        this.elevatorPosition = elevatorPosition;
+        this.height = height;
+
+        requires(elevator);
+    }
+
+    protected void initialize(){
+        isAtPosition = false;
+    }
+
+    protected void execute(){
+        if(elevatorPosition.getHeight() < height){
+            elevator.goUp(speed);
+        } else if (elevatorPosition.getHeight() > height) {
+            elevator.goDown(speed);
+        } else {
+            elevator.goUp(sustainSpeed);
+        }
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return isAtPosition;
+    }
 }
