54d575d4cee8a6363ac3371f090cdab68d9a512a
Chris Russell <crussell@sonnysdirect.com>
2018-03-30 07:49:38 -0400

    Adjust elevator power/duration for auto positioning for switch

--- src/main/java/org/usfirst/frc/team1251/robot/commands/TimedElevatorUpAndHold.java
+++ src/main/java/org/usfirst/frc/team1251/robot/commands/TimedElevatorUpAndHold.java
@@ -6,20 +6,21 @@ import org.usfirst.frc.team1251.robot.subsystems.Elevator;
 public class TimedElevatorUpAndHold extends TimedCommand {
 
     private Elevator elevator;
+    private final double speed;
 
-    private static double SPEED = .50;
     private static double SUSTAIN_SPEED = .1;
 
-    public TimedElevatorUpAndHold(Elevator elevator, double timeout) {
-        super(timeout);
+    public TimedElevatorUpAndHold(Elevator elevator, double duration, double speed) {
+        super(duration);
         this.elevator = elevator;
+        this.speed = speed;
 
         requires(elevator);
     }
 
 
     protected void execute(){
-        elevator.goUp(SPEED);
+        elevator.goUp(speed);
     }
 
     @Override
