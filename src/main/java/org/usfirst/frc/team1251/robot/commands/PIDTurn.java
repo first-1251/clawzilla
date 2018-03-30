09ef83afbd5bc03de4c0adc9f62b45f1caa95cf1
Chris Russell <crussell@sonnysdirect.com>
2018-03-30 15:02:39 -0400

    Add timeout to PIDTurn

--- src/main/java/org/usfirst/frc/team1251/robot/commands/PIDTurn.java
+++ src/main/java/org/usfirst/frc/team1251/robot/commands/PIDTurn.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team1251.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.PIDCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import org.usfirst.frc.team1251.robot.subsystems.DriveTrain;
 import org.usfirst.frc.team1251.robot.virtualSensors.DriveFeedback;
 
@@ -10,6 +12,11 @@ public class PIDTurn extends PIDCommand {
     private static final double I = 0.000015;
     private static final double D = 0.0;
 
+    private final double timeAllowance;
+    private final Timer timeAllowanceTimer = new Timer();
+
+    private final static double TIMED_OUT_THRESHOLD = 3;
+    private final static double TIME_ALLOWANCE = 2;
 
     enum Direction {
         CLOCKWISE, COUNTER_CLOCKWISE
@@ -26,6 +33,7 @@ public class PIDTurn extends PIDCommand {
     public PIDTurn(DriveTrain driveTrain, DriveFeedback driveFeedback, double desiredAngle) {
         super(P, I, D, PERIOD);
 
+        this.timeAllowance = TIME_ALLOWANCE;
         this.desiredAngle = desiredAngle;
         this.driveTrain = driveTrain;
         this.driveFeedback = driveFeedback;
@@ -37,6 +45,12 @@ public class PIDTurn extends PIDCommand {
 
     @Override
     protected void initialize() {
+        this.timeAllowanceTimer.stop();
+        this.timeAllowanceTimer.reset();
+        if (this.timeAllowance > 0) {
+            this.timeAllowanceTimer.start();
+        }
+
         double distanceToTarget = distanceToTarget();
         this.getPIDController().setSetpoint(desiredAngle);
 
@@ -55,6 +69,7 @@ public class PIDTurn extends PIDCommand {
 
     @Override
     protected void usePIDOutput(double output) {
+
         if (direction == Direction.COUNTER_CLOCKWISE) {
             driveTrain.setSpeed(-output, output);
         } else {
@@ -80,6 +95,28 @@ public class PIDTurn extends PIDCommand {
 
     @Override
     protected boolean isFinished() {
+
+        if (timeAllowance > 0 && timeAllowanceTimer.get() >= timeAllowance) {
+            // Times up! See how we did.
+            if (Math.abs(this.desiredAngle - this.distanceToTarget()) <= TIMED_OUT_THRESHOLD) {
+                // Close enough! Short circuit the command.
+                return true;
+            }
+        }
+
         return getPIDController().onTarget();
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        super.initSendable(builder);
+    }
+
+    @Override
+    protected void end() {
+        super.end();
+        this.timeAllowanceTimer.stop();
+        this.timeAllowanceTimer.reset();
+        this.driveTrain.setSpeed(0, 0);
     }
 }
