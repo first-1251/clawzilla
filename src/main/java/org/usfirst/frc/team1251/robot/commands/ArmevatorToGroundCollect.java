8c3b560dec9e131c07b47a6028479495d51b1d0f
Chris Russell <crussell@sonnysdirect.com>
2018-04-18 16:48:10 -0500

    Move elevator down before arm when moving it to ground-collect position
    This gives it enough time to clear the switch.

--- src/main/java/org/usfirst/frc/team1251/robot/commands/ArmevatorToGroundCollect.java
+++ src/main/java/org/usfirst/frc/team1251/robot/commands/ArmevatorToGroundCollect.java
@@ -10,7 +10,7 @@ import org.usfirst.frc.team1251.robot.virtualSensors.ElevatorPosition;
 public class ArmevatorToGroundCollect extends CommandGroup {
     public ArmevatorToGroundCollect(double delay, Elevator elevator, ElevatorPosition elevatorPosition, Arm arm, ArmPosition armPosition) {
         addSequential(new TimedNothing(delay));
-        addParallel(new ElevatorToBottom(elevator, elevatorPosition));
-        addParallel(new AutoArmDownToMinimum(arm, armPosition));
+        addSequential(new ElevatorToBottom(elevator, elevatorPosition));
+        addSequential(new AutoArmDownToMinimum(arm, armPosition));
     }
 }
