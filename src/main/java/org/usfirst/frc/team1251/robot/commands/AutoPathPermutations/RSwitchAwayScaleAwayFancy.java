99ba54466a645ed3ea5ca5868c921c1d55017c9e
Nick Salazar <koipasta3@gmail.com>
2018-04-20 08:17:07 -0500

    Cross the line when unfavorable on right

---
+++ src/main/java/org/usfirst/frc/team1251/robot/commands/AutoPathPermutations/RSwitchAwayScaleAwayFancy.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team1251.robot.commands.AutoPathPermutations;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc.team1251.robot.commands.*;
+import org.usfirst.frc.team1251.robot.subsystems.*;
+import org.usfirst.frc.team1251.robot.virtualSensors.ArmPosition;
+import org.usfirst.frc.team1251.robot.virtualSensors.DriveFeedback;
+import org.usfirst.frc.team1251.robot.virtualSensors.ElevatorPosition;
+
+public class RSwitchAwayScaleAwayFancy extends CommandGroup {
+    public RSwitchAwayScaleAwayFancy(Elevator elevator, ElevatorPosition elevatorPosition, Arm arm, ArmPosition armPosition, Claw claw, Collector collector, DriveFeedback driveFeedback, DriveTrain driveTrain, DriveTrainShifter driveTrainShifter){
+
+        addSequential(new CrossLineAuto(driveTrain, driveFeedback, driveTrainShifter));
+
+    }
+
+}
